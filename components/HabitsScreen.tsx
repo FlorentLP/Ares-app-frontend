3876431741b9203f74c4028bb69e64410f94d0b1
florent <florent.le.pape@gmail.com>
2024-12-07 09:41:27 +0100

    add habbit list

--- components/HabitsScreen.tsx
+++ components/HabitsScreen.tsx
@@ -1,11 +1,13 @@
 import MyFAB from "./MyFAB";
 import HabitList from "./HabitList";
+import HabitEditForm from "./HabitEditForm";
 
 export default function HabitsScreen() {
   return (
     <>
       <HabitList></HabitList>
       <MyFAB></MyFAB>
+      <HabitEditForm></HabitEditForm>
     </>
   );
 }
